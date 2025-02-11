ee879783b129cba9c73721549f069299654ff024
Vanuiy <vanya93shevchenko@gmail.com>
2025-02-11 18:35:41 +0200

    Black and white themes

--- front/src/components/Vanya/Vanya.jsx
+++ front/src/components/Vanya/Vanya.jsx
@@ -1,5 +1,18 @@
+const inner = document.getElementById('inner')
 import {Button, TextField } from "@mui/material";
 import React, { useState } from "react";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+export function SwitchLabels() {
+    return (
+      <FormGroup>
+        <FormControlLabel control={<Switch defaultChecked />} label="Label" />
+        <FormControlLabel required control={<Switch />} label="Required" />
+        <FormControlLabel disabled control={<Switch />} label="Disabled" />
+      </FormGroup>
+    );
+  }
 
 const Vanya = () => {
 const [state, setState] = useState({
@@ -10,6 +23,14 @@ const [state, setState] = useState({
 const send = () =>{
     console.log(state);
     
+}
+
+const Change = () =>{
+    if(inner.style.backgroundColor == "rgb(39, 39, 39)"){
+        inner.style.backgroundColor = "white"
+    }else{
+        inner.style.backgroundColor = "rgb(39, 39, 39)"
+    }
 }
 
     return (
@@ -23,6 +44,7 @@ const send = () =>{
                     setState({...state,password: e.target.value})
                 }}/>
                 <Button variant="contained"onClick={send}>Enter</Button>
+                <FormControlLabel onClick={Change} control={<Switch defaultChecked />} label="" />
             </div>
         </div>
     )

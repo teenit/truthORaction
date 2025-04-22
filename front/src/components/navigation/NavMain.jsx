923b942a579d31e5063bd8a5126c255a17f771b8
Pavlo <teenitclub@gmail.com>
2025-04-22 20:25:35 +0300

    Auto stash before merge of "main" and "origin/main"

--- front/src/components/navigation/NavMain.jsx
+++ front/src/components/navigation/NavMain.jsx
@@ -1,18 +1,41 @@
-import { Button } from "@mui/material";
-import React from "react";
-import { FaUser } from "react-icons/fa";
+import { Button, Menu, MenuItem } from "@mui/material";
+import React, { useState } from "react";
+import { FaUser, FaCog } from "react-icons/fa";
 import { NavLink, useNavigate } from "react-router-dom";
 import './NavMain.scss';
 
 const NavMain = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [openSettings, setOpenSettings] = useState(false)
 
-    return(
-        <div>
-            <Button onClick={() =>{navigate("/")}} >Home</Button>
-            <Button onClick={() =>{navigate("/vanya")}} > <FaUser /> Vaniy</Button>
-            <Button onClick={() =>{navigate("/denis")}} >Denis</Button>
-            <Button onClick={() =>{navigate("/pavlo")}} >Pavlo</Button>
+    return (
+        <div className="NavMain">
+            <div className="NavMain-left">
+                <Button onClick={() => { navigate("/") }} >Home</Button>
+                <Button onClick={() => { navigate("/vanya") }} > <FaUser /> Vaniy</Button>
+                <Button onClick={() => { navigate("/denis") }} >Denis</Button>
+                <Button onClick={() => { navigate("/pavlo") }} >Pavlo</Button>
+            </div>
+            <div className="NavMain-right">
+                <Button
+                    id="basic-button"
+                    aria-controls={openSettings ? 'basic-menu' : undefined}
+                    onClick={() => {
+                        setOpenSettings(!openSettings)
+                    }}
+                >
+                    <FaCog />
+                </Button>
+            {openSettings && 
+             <div className="NavMain-right-menu">
+                <p>Item 1</p>
+                <p>Item reker geriugberuig eiuhufre ireugierhgue rgueirghieurhger g erugiheruighuerhg 2</p>
+                <p>Item 3</p>
+                <p>Item 4</p>
+            </div>
+            }
+           
+            </div>
             <div className="line"></div>
         </div>
     )
